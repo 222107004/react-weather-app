7cc03f7aecc06a005f36f9c3296da1f21f750dee
222107004 <thama.mabatha@gmail.com>
2024-08-11 00:20:07 +0200

    Added github repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by Thama Mabatha and is {""}
+        <a
+          href="https://github.com/222107004/react-weather-app"
+          target="_blank"
+        >
+          open sourced on github
+        </a>
+      </footer>
+    </div>
+  );
 }
