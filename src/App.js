5657bb61bb4146cf41ca096b940f7f9d7340d0e6
222107004 <thama.mabatha@gmail.com>
2024-08-11 17:12:15 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
+
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <Weather />
       <footer>
         This project is created by Thama Mabatha and is {""}
-        <a
-          href="https://github.com/222107004/react-weather-app"
-          target="_blank"
-        >
+        <a href="https://github.com/222107004/react-weather-app">
           open sourced on github
         </a>
       </footer>
